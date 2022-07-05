db04ef9bb8bf44c921dda78e4404012759d3d757
unknown <k-b-v@yandex.ru>
2022-07-05 17:37:17 +0300

    code

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,10 @@ public class Main {
         int bill = 200;
         int plus = 1000;
 
-        if (bill+plus > 1000) {
-            System.out.println("Сумма бонусов: " + (bill + plus)  / 100 );
-     }
+        if (bill + plus > 1000) {
+            System.out.println("Сумма бонусов: " + (bill + plus) / 100);
+            System.out.println("Итоговый счет: " + (bill + plus));
+        }
     }
 }
 
