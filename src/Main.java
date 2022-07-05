af40225cb59617e1c54328584e6c8f4f7a5f2041
unknown <k-b-v@yandex.ru>
2022-07-05 17:29:56 +0300

    papka

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int bill = 200;
+        int plus = 1000;
+
+        if (bill+plus > 1000) {
+            System.out.println("Сумма бонусов: " + (bill + plus)  / 100 );
+     }
+    }
+}
+
